5fb72fec69eeb3188b0bce857b59a779596c687f
Harsh <thakur2004harsh@gmail.com>
2025-01-24 18:13:57 +0530

    ENV Updated

--- src/app/api/contact/route.ts
+++ src/app/api/contact/route.ts
@@ -40,7 +40,7 @@ export async function POST(request: Request) {
       }),
       resend.emails.send({
         from: 'Black Mining <onboarding@resend.dev>',
-        to: 'blackminingjunk@gmail.com',
+        to: process.env.CONTACT_EMAIL || '',
         subject: `New Contact Request - ${ownerName}`,
         react: ContactFormEmail({
           formData: { ownerName, email, phoneNumber, vehicle, message, formSource }
