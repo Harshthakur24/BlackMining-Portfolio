39f5804abbddd50203382f5f42e5359b72df731e
Harsh <thakur2004harsh@gmail.com>
2025-03-03 23:00:04 +0530

    Updated

--- src/app/layout.js
+++ src/app/layout.js
@@ -10,6 +10,17 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+        <script async src="https://www.googletagmanager.com/gtag/js?id=AW-10806809101">
+        </script>
+        <script>
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments)}
+          gtag('js', new Date());
+
+          gtag('config', 'AW-10806809101');
+        </script>
+      </head>
       <body
         className={` antialiased`}
       >
