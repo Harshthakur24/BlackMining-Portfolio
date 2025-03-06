6c216f14e55ba1b55aba76a4352828563ff92a10
Harsh <thakur2004harsh@gmail.com>
2025-03-06 22:59:10 +0530

    Updated

--- src/app/layout.js
+++ src/app/layout.js
@@ -27,6 +27,22 @@ export default function RootLayout({ children }) {
             gtag('config', 'AW-10806809101');
           `}
         </Script>
+
+        {/* New Google Tag (gtag.js) */}
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=AW-16912183761"
+          strategy="afterInteractive"
+        />
+        <Script id="google-tag-16912183761" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+
+            gtag('config', 'AW-16912183761');
+          `}
+        </Script>
+
         <Navbar />
         {children}
         <Footer />
